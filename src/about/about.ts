4b98a66ae30ea11aa9d0f53caca9c70250bbc972
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-13 17:58:29 -0500

    fix links

--- src/about/about.ts
+++ src/about/about.ts
@@ -17,7 +17,7 @@ export class aboutComponent{
     specialtiesText = `specialtiesText placeholder`;
 
     contactLinks = [
-        { label: 'Contact Me', href: 'mailto:dkaing484@gmail.com'},
+        { label: 'Contact Me', href: 'https://mail.google.com/mail/?view=cm&fs=1&to=dkaing484@gmail.com'},
         { label: 'LinkedIn', href: 'https://www.linkedin.com/in/dylan-kaing-25a7b9210/'},
         { label: 'GitHub', href:'https://github.com/DylanKaing'}
     ]
