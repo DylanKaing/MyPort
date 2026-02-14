7130002245859841cbfb6c4a50fc8e7509da9a79
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-13 20:56:00 -0500

    add skills

--- src/about/about.ts
+++ src/about/about.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'about',
     templateUrl: './about.html',
-    styleUrls: ['./about.css']
+    styleUrls: ['./about.css'],
+    imports: [CommonModule]
 })
 export class aboutComponent{
     name = 'Dylan Kaing';
     role = 'Software Developer';
     location = 'Pittsburgh and Philadelphia, PA';
 
-    aboutText = `aboutText placeholder`;
+    aboutText = `Welcome to my portfolio! Here you can find various things from skills, projects, transcript and even my resume.`;
 
     bioText = `bioText placeholder`;
 
@@ -21,4 +23,54 @@ export class aboutComponent{
         { label: 'LinkedIn', href: 'https://www.linkedin.com/in/dylan-kaing-25a7b9210/'},
         { label: 'GitHub', href:'https://github.com/DylanKaing'}
     ]
+
+    skills = {
+        technical: [
+            'Java',
+            'C',
+            'CSS',
+            'HTML',
+            'R',
+            'Assembly(MIPS)',
+            'JavaScript',
+            'Python',
+            'Rust',
+            'SQL',
+            'TypeScript',
+            'Flask',
+            'Jinja',
+            'Node.js',
+            'SpringBoot',
+            'Angular'
+        ],
+        tools:[
+            'Git',
+            'Figma',
+            'VSCode',
+            'Docker',
+            'Selenium',
+            'JUnit',
+            'QuickTest',
+            'Cucumber',
+            'Google Address Sanitzer',
+            'Google Thread Sanitizer',
+            'SpotBugs',
+            'StyleChecker',
+            'Java Path Finder',
+            'Visual VM',
+            'Swagger UI',
+            'Postman',
+            'Maven',
+            'AWS'
+
+        ],
+        soft:[
+            'Problem Solving',
+            'Communication',
+            'Teamwork',
+            'Testing & QA',
+            'Debugging',
+            'System Design'
+        ]
+    }
 }
