31cd4c363bce6d85c6fa7171690fe9bffdd2b8e2
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-15 18:26:32 -0500

    add routerlink instead of href

--- src/about/about.ts
+++ src/about/about.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'about',
     templateUrl: './about.html',
     styleUrls: ['./about.css'],
     standalone: true,
-    imports: [CommonModule]
+    imports: [CommonModule, RouterModule]
 })
 export class AboutComponent{
     name = 'Dylan Kaing';
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { AboutComponent } from '../about/about';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent],
+  imports: [RouterOutlet, AboutComponent, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
