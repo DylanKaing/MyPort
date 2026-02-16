e1a37e42e79f44a7b6ff69818736713cb0d0840d
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-15 19:09:48 -0500

    Add pdf-viewer for resume

--- src/resume/resume.ts
+++ src/resume/resume.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { RouterModule } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'resume',
     templateUrl: './resume.html',
     styleUrls: ['./resume.css'],
     standalone: true,
-    imports: [CommonModule, NgxExtendedPdfViewerModule, RouterModule]
+    imports: [CommonModule,RouterModule]
 })
 export class ResumeComponent{
+
+    pdfUrl: SafeResourceUrl;
+
+    constructor(private sanitizer: DomSanitizer) {
+        this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/DylanKaing2026Resume.pdf');
+    }
     
 }
