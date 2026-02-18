9e32f8e3ebb981e2082a0155e49efcf80559eef7
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-18 12:18:16 -0500

    Update projects.ts

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -36,7 +36,7 @@ export class ProjectsComponent{
         },
         {
             id: 4,
-            title: 'CS Job Webscrapper',
+            title: 'CS Job Finder',
             description: 'In progress',
             tags: [],
             github: ''
