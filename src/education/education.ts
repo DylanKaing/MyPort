b36800dfc80ae2c0216d81a07bf766d220a1e3e7
DylanKaing <101293746+DylanKaing@users.noreply.github.com>
2026-02-18 03:12:31 -0500

    Add education page

--- src/education/education.ts
+++ src/education/education.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
     selector: 'education',
@@ -10,5 +12,13 @@ import { RouterModule } from '@angular/router';
     imports: [CommonModule, RouterModule]
 })
 export class EducationComponent{
+
+    transcriptUrl: SafeResourceUrl;
+    degreeUrl: SafeResourceUrl;
+
+    constructor(private sanitizer: DomSanitizer){
+        this.transcriptUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/1_14_2026Transcript.pdf');
+        this.degreeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/degree.pdf');
+    }
     
 }
